afdaac9ad6cf66bbb95be1a792c6b29a55e6b56b
4Kirb <30355227+4Kirb@users.noreply.github.com>
2022-11-27 16:38:20 +0000

    added proper file reading

--- recipeviewer.c
+++ recipeviewer.c
@@ -1,22 +1,52 @@
 static int system();
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 int main(){
-	char filename[100];
-	printf("Enter file path: ");
-	scanf("%s",filename);
-	char cat[] = "cat -b ";
-	strcat(cat,filename);
+	char filename[100], cat[100],sed[100];
+	strcpy(filename, "coqauvin");
+	strcpy(cat, "cat -b ");
+	strcpy(sed, "sed -n 1p ");
+	strcat(cat, filename);
+	strcat(sed, filename);
+  int i = 1;
 	system(cat);
-	
-	printf( "Enter a line number: ");
-	int i;
-	
-	scanf("%d", &i);
-	char greetings[] = "sed -n 1p coqauvin";
-	system(greetings);
-	//Read the line in C, find out the current measurement 
+	system(sed);
+	//Read the line in C, find out the current measurement
+	FILE *fp;
+	char * line = NULL;
+  size_t len = 0;
+  ssize_t read;
+
+	fp = fopen(filename, "r"); // read mode
+	if (fp == NULL)
+   {
+      perror("Error while opening the file.\n");
+      exit(EXIT_FAILURE);
+   }
+	int j = 0;
+	while ((read = getline(&line, &len, fp)) != -1) {
+				j++;
+				if(j==i){
+				break;
+				}
+  }
+	printf("%s", line);
+  /*
+	Measurements to search for:
+	g
+	Kg
+	Oz
+	Lb
+  Ml
+	L
+	Cup
+	Tbsp
+	Tsp
+	Floz
+	 */
+  fclose(fp);
 	//Ask for new measurement
 	//Convert
 	//Display
